711c62100f4cbdc6e390d870a3eee46b5c3eed55
ACEfanatic02 <acefanatic02@gmail.com>
2013-05-05 03:44:56 -0400

    test program

--- src/test4241.c
+++ src/test4241.c
@@ -2,12 +2,26 @@
 
 #include "4241.h"
 
-int main(int argc, char const *argv[]) {
+void print_results(processor * p) {
+    printf("Final state:\n");
+    printf("r0: %X\tr1: %X\n", p->r0, p->r1);
+
+    printf("Memory:\n");
+    int i;
+    for (i = 0; i < 16; ++i) {
+        printf("%X ", p->mem[i]);
+        if ((i + 1) % 4 == 0) {
+            printf("\n");
+        }
+    }
+}
+
+int main(void) {
     
     byte memory[16] = {
-        0x0,0x0,0x0,0x0,
-        0x0,0x0,0x0,0x0,
-        0x0,0x0,0x0,0x0,
+        0x3,0x3,0x3,0x4,
+        0x4,0x1,0xB,0x9,
+        0x8,0x0,0x0,0x0,
         0x0,0x0,0x0,0x0
     };
 
@@ -16,6 +30,8 @@ int main(int argc, char const *argv[]) {
 
     if (results == NULL) {
         printf("An error occurred running the program.\n");
+    } else {
+        print_results(results);
     }
 
     return 0;
